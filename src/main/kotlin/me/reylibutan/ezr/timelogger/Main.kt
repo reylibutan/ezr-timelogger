3603b2fdf981c111b8f82a1b3c73e778ad1f2c69
reylibutan <reylibutan@gmail.com>
2021-12-03 23:27:44 +0800

    replace submit with preview

--- src/main/kotlin/me/reylibutan/ezr/timelogger/Main.kt
+++ src/main/kotlin/me/reylibutan/ezr/timelogger/Main.kt
@@ -5,8 +5,8 @@ import me.reylibutan.ezr.timelogger.service.DsrService
 import me.reylibutan.ezr.timelogger.service.TimeEntryService
 
 fun main() {
-  val csvLines = DsrService().dsrToCsv("D:\\___\\_lol\\ezr-timelogger\\src\\main\\resources\\timesheet_nov_w5_2021.dsr")
-//  val csvLines = DsrService().dsrToCsv("/Users/reylibutan/IdeaProjects/ezr-timelogger/src/main/resources/timesheet_nov_w1_2021.dsr")
+//  val csvLines = DsrService().dsrToCsv("D:\\___\\_lol\\ezr-timelogger\\src\\main\\resources\\timesheet_nov_w5_2021.dsr")
+  val csvLines = DsrService().dsrToCsv("/Users/reylibutan/IdeaProjects/ezr-timelogger/src/main/resources/timesheet_nov_w1_2021.dsr")
   val csvEntries = csvReader().readAllWithHeader(csvLines.joinToString("\n"))
-  TimeEntryService().submitTimeEntries(csvEntries)
+  TimeEntryService().previewTimeEntries(csvEntries)
 }
