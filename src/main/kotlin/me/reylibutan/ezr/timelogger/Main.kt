e1e029444b2f96d692b8c826f97f34e66b42465e
reylibutan <reylibutan@gmail.com>
2022-04-23 19:30:27 +0800

    Updates for April

--- src/main/kotlin/me/reylibutan/ezr/timelogger/Main.kt
+++ src/main/kotlin/me/reylibutan/ezr/timelogger/Main.kt
@@ -5,7 +5,7 @@ import me.reylibutan.ezr.timelogger.service.DsrService
 import me.reylibutan.ezr.timelogger.service.TimeEntryService
 
 fun main() {
-  val csvLines = DsrService().dsrToCsv("src/main/resources/2022_03/timesheet_mar_w5_2022.dsr")
+  val csvLines = DsrService().dsrToCsv("src/main/resources/2022_04/timesheet_apr_w4_2022.dsr")
   val csvEntries = csvReader().readAllWithHeader(csvLines.joinToString("\n"))
   TimeEntryService().submitTimeEntries(csvEntries)
 }
